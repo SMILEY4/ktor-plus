5f4e608e7b94c569ad1ab63a4b66e130ba6a3589
Lukas Ruegner <lukas.ruegner@xitaso.com>
2025-12-23 19:30:27 +0100

    configure project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,90 @@
-plugins {
-    kotlin("jvm") version "2.2.10"
-}
+import com.vanniktech.maven.publish.KotlinJvm
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.dokka.gradle.DokkaTask
+import com.vanniktech.maven.publish.JavadocJar
 
-group = "io.github.smiley4"
-version = "1.0-SNAPSHOT"
+val projectGroupId: String by project
+val projectVersion: String by project
+group = projectGroupId
+version = projectVersion
+
+plugins {
+    kotlin("jvm") version "2.2.21"
+    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.owasp.dependencycheck") version "8.2.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+    id("com.vanniktech.maven.publish") version "0.33.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(11)
+}
+
+detekt {
+    ignoreFailures = false
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$projectDir/detekt/detekt.yml")
+}
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        md.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    outputDirectory.set(file("$rootDir/docs/dokka/ktor-openapi"))
+}
+
+mavenPublishing {
+    val projectGroupId: String by project
+    val projectArtifactId: String by project
+    val projectVersion: String by project
+    val projectBaseScmUrl: String by project
+    val projectBaseScmConnection: String by project
+    val projectLicenseName: String by project
+    val projectLicenseUrl: String by project
+    val projectDeveloperName: String by project
+    val projectDeveloperUrl: String by project
+
+    configure(KotlinJvm(JavadocJar.Dokka("dokkaHtml"), true))
+    publishToMavenCentral(automaticRelease = true)
+//    signAllPublications()
+    coordinates(projectGroupId, projectArtifactId, projectVersion)
+    pom {
+        name.set("Ktor-Plus")
+        description.set("Kotlin library that provides enhanced functionality on top of Ktor.")
+        url.set(projectBaseScmUrl)
+        licenses {
+            license {
+                name.set(projectLicenseName)
+                url.set(projectLicenseUrl)
+                distribution.set(projectLicenseUrl)
+            }
+        }
+        scm {
+            url.set(projectBaseScmUrl)
+            connection.set(projectBaseScmConnection)
+        }
+        developers {
+            developer {
+                id.set(projectDeveloperName)
+                name.set(projectDeveloperName)
+                url.set(projectDeveloperUrl)
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,12 @@
+rootProject.name = "ktor-plus"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "ktor-plus"
